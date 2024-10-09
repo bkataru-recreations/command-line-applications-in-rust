ad99ece923872c0564d1df4075e06235031ad564
BK-Modding <kavesbteja@gmail.com>
2024-10-09 05:38:02 +0530

    exercise solved: using buffered i/o for optimized file operations

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,16 +1,29 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
 
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let file = File::open(args.path).expect("could not open file");
+    let mut reader = BufReader::new(file);
+    let mut string = String::new();
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    while reader.read_line(&mut string).unwrap() > 0 {
+        if string.contains(&args.pattern) {
+            println!("{}", string);
+        }
+
+        string.clear();
+    }
 }

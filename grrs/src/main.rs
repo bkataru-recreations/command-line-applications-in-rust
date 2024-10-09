1117feb2760b53f56d780a483fdc3ccf1f259ee2
BK-Modding <kavesbteja@gmail.com>
2024-10-09 16:10:07 +0530

    progress bars with indicatif

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -13,6 +13,14 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
+    let pb = indicatif::ProgressBar::new(100);
+
+    for i in 0..100 {
+        pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+
     let args = Cli::parse();
 
     let content = std::fs::read_to_string(&args.path)
